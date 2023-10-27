eccc7181eebc98d56f3d297e7d83939088180797
sunejahardik <132489120+sunejahardik@users.noreply.github.com>
2023-10-27 16:14:24 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+// pehle arrow use nhi hote thai ab sb hote hai
+"use strict"; //treat all JS code as newer version
+//alert("hello"); // we are using node js,not browser 
+//mostly semicolons are avoided in react and node js
+//readability is very important
+console.log(3+3)
+console.log("hardik")
+let name="hardik"//string
+let age =18//number
+let isLoggedIn=true///boolean
+//primitive data types=
+//number ka range bhi hai=>2 to power 53
+//biginy big numbers kai liye
+//string="" can  be used in both single and double inverted commas
+//boolean=true or false 
+//null=stand alone value,empty(khaali hai)
+//undefined=variable declare krra pr koi value nhi di
+//symbol=unique
+console.log(typeof null)//object
+//type of null dekha to object aaya issue hai
+console.log(typeof undefined)//undefined
